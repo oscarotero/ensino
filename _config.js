c1cf0b7d169f9a5720f1c316df815bbd136af24a
Oscar Otero <oom@oscarotero.com>
2021-03-10 20:52:40 +0100

    reemplazado 11ty por lume

---
+++ _config.js
@@ -0,0 +1,15 @@
+import lume from "https://deno.land/x/lume@v0.15.4/mod.js";
+
+const site = lume({
+  src: ".",
+  dest: "_site",
+});
+
+site.ignore("README.md");
+
+site.copy("favicon.ico");
+site.copy("static");
+site.copy("admin");
+site.copy("_includes/assets/");
+
+export default site;
