e57d8114db112c95b188699ed2427d8d47211e9d
Oscar Otero <oom@oscarotero.com>
2022-05-27 19:57:05 +0200

    actualización

---
+++ _data/netlify_cms.ts
@@ -0,0 +1,49 @@
+import f from "https://deno.land/x/netlify_cms_config@v0.1.0/mod.ts";
+
+f.defaultRequired = false;
+
+const categorias = f.folder("Categorías", "/categories", [
+  f.string("Nombre").name("title"),
+  f.string("Identificador").name("id"),
+  f.relation("Categorías nas que aparece")
+    .name("tags")
+    .collection("categorías")
+    .valueField("id")
+    .displayFields(["title"])
+    .searchFields(["title"])
+    .multiple(),
+  f.markdown("Corpo").name("body"),
+])
+  .create()
+  .slug("{{slug}}");
+
+const paxinas = f.folder("Páxinas", "/pages", [
+  f.string("Título").name("title"),
+  f.markdown("Corpo").name("body"),
+])
+  .create()
+  .slug("{{slug}}");
+
+const exercicios = f.folder("Exercicios", "/posts", [
+  f.string("Título").name("title"),
+  f.text("Sumario").name("summary"),
+  f.relation("Categorías nas que aparece", categorias, "id").multiple(),
+  f.boolean("Ocultar").name("draft").default(false),
+  f.markdown("Corpo").name("body"),
+])
+  .create()
+  .slug("{{slug}}");
+
+export default {
+  backend: {
+    name: "git-gateway",
+    branch: "master",
+  },
+  media_folder: "img",
+  public_folder: "/img",
+  collections: [
+    exercicios.toJSON(),
+    paxinas.toJSON(),
+    categorias.toJSON(),
+  ],
+};
