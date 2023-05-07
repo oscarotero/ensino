cc54e4c64cbef4fe60d892b142a0287974d29988
Oscar Otero <oom@oscarotero.com>
2023-05-07 13:57:00 +0200

    melloras

--- _config.ts
+++ _config.ts
@@ -3,6 +3,7 @@ import slugifyUrls from "lume/plugins/slugify_urls.ts";
 import inline from "lume/plugins/inline.ts";
 import pagefind from "lume/plugins/pagefind.ts";
 import netlifyCms from "lume/plugins/netlify_cms.ts";
+import toc from "https://deno.land/x/lume_markdown_plugins@v0.5.0/toc.ts";
 
 const site = lume({
   src: ".",
@@ -18,6 +19,7 @@ site.ignore("README.md")
     netlifyIdentity: true,
     previewStyle: "admin/admin.css",
   }))
+  .use(toc())
   .copy("admin")
   .copy("admin.css", "admin/admin.css")
   .copy("favicon.ico")
