7c50794d731c975b4d240064cde86db32f7618a2
Oscar Otero <oom@oscarotero.com>
2024-01-29 15:39:23 +0100

    actualizado lume

--- _config.ts
+++ _config.ts
@@ -1,7 +1,6 @@
 import lume from "lume/mod.ts";
 import slugifyUrls from "lume/plugins/slugify_urls.ts";
 import inline from "lume/plugins/inline.ts";
-import decapCMS from "lume/plugins/decap_cms.ts";
 import toc from "https://deno.land/x/lume_markdown_plugins@v0.6.0/toc.ts";
 import nunjucks from "lume/plugins/nunjucks.ts";
 
@@ -17,11 +16,6 @@ site.ignore("README.md")
   }))
   .use(inline())
   .use(nunjucks())
-  .use(decapCMS({
-    identity: "netlify",
-    previewStyle: "admin/admin.css",
-    proxyCommand: "npx decap-server"
-  }))
   .use(toc())
   .copy("admin")
   .copy("admin.css", "admin/admin.css")
