b65367dfdfffc23c04ac9cd23dbc4a7810ca65dc
Oscar Otero <oom@oscarotero.com>
2024-01-29 15:58:49 +0100

    probando cms

---
+++ _cms.ts
@@ -0,0 +1,37 @@
+import lume from "cms/adapters/lume.ts";
+
+const app = await lume({
+  configFile: "_config.ts",
+});
+
+app.storage("fs");
+
+app.collection(
+  "posts",
+  "fs:posts/*.md",
+  [
+    "title: text",
+    "summary: textarea",
+    "tags: list",
+    "image: file",
+    "draft: checkbox",
+    "show_toc: checkbox",
+    "content: markdown",
+  ],
+);
+
+app.collection("paxinas", "fs:pages", [
+  "title: text",
+  "url: text",
+  "content: markdown",
+]);
+
+app.collection("categorías", "fs:categories", [
+  "title: text",
+  "id: text",
+  "tags: list",
+]);
+
+app.upload("imaxes", "fs:img");
+
+app.serve();
